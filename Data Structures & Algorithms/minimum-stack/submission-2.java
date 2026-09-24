dad0a500b6e45b5442a35526e9c22fdeb3df45b4
Udbhav Deva <udbhavdeva1@gmail.com>
2026-09-24 23:47:09 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.java
@@ -0,0 +1,27 @@
+class MinStack {
+    Stack<Integer> stack = new Stack<>();
+    Stack<Integer> minStack = new Stack<>();
+    
+    public void push(int val) {
+        stack.push(val);
+        if(minStack.empty()){
+            minStack.push(val);
+        }
+        else{
+            minStack.push(Math.min(minStack.peek(), val));
+        } 
+    }
+    
+    public void pop() {
+        stack.pop();
+        minStack.pop(); 
+    }
+    
+    public int top() {
+       return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
